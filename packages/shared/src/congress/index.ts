e41ed8825a055cf0b4569f93604824ae5e0d74cb
123nnymus123-a11y <123.nnymus.123@gmail.com>
2026-04-01 20:25:52 +0200

    Delete packages directory

--- packages/shared/src/congress/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./congressionalTrade";
-export * from "./congressionalMember";
-export * from "./lobbyingActivity";
-export * from "./federalContract";
-export * from "./companyTickerMapping";
-export * from "./dataIngestionLog";
--- packages/shared/src/publicFlow/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./disclosureEvent";
-export * from "./sectorTheme";
-export * from "./watchlistCandidate";
-export * from "./valuationTag";
